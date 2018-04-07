7be5a058e3eadc876aaca328eef2a77a3623cae6
Thesharing <cyprestar@outlook.com>
2018-04-07 22:35:22 +0800

    Ver. 0.3

--- fanatical-price-checker.js
+++ fanatical-price-checker.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   Fanatical 跨区价格查询
 // @name:zh-TW   Fanatical 跨區價格查詢
 // @namespace    https://www.fanatical.com/
-// @version      0.2
+// @version      0.3
 // @description  Check the price of different regions for Fanatical.
 // @description:zh-CN 查询不同区域的Fanatical游戏/慈善包的价格
 // @description:zh-TW 查詢不同區域的Fanatical遊戲/慈善包的價格
@@ -11,11 +11,12 @@
 // @downloadURL  https://github.com/Thesharing/fanatical-price-checker/raw/master/fanatical-price-checker.js
 // @updateURL    https://github.com/Thesharing/fanatical-price-checker/raw/master/fanatical-price-checker.ver.js
 // @author       Thesharing
+// @license      MIT
 // @include      /^https?://www\.fanatical\.com/en/bundle/.+$
 // @include      /^https?://www\.fanatical\.com/en/game/.+$
+// @include      /^https?://www\.fanatical\.com/en/dlc/.+$
 // @grant        GM_xmlhttpRequest
 // @run-at       document-idle
-
 // ==/UserScript==
 
 var totalNum = 0;
@@ -23,6 +24,7 @@ var priceList = [];
 var regionList = ["USD", "CAD", "EUR", "GBP"];
 var curList = {};
 var urlType = document.URL.split('/')[4];
+var targetCur = "CNY";
 
 (function () {
     'use strict';
@@ -33,14 +35,29 @@ var urlType = document.URL.split('/')[4];
         childList: true,
         characterData: true
     };
-    var subObserver = new MutationObserver(function (mutations, me) {
-        var a = document.getElementsByClassName('product-commerce-container');
-        if (a.length > 0) {
-            addButton(a[0]);
-            me.disconnect();
+    var continuousObserver = new MutationObserver(function (mutations, me) {
+        var normalPageContainer = document.getElementsByClassName('product-commerce-container');
+        var starDealPageContainer = document.getElementsByClassName('purchase-info-container');
+        if (starDealPageContainer.length > 0){
+            console.log('STARDEAL');
+            addStarDealButton(starDealPageContainer[0]);
+        }
+        if (normalPageContainer.length > 0) {
+            console.log('NORMAL');
+            addNormalButton(normalPageContainer[0]);
         }
     });
 
+    var subObserver = new MutationObserver(function (mutations, me) {
+        var normalPageContainer = document.getElementsByClassName('product-commerce-container');
+        if (normalPageContainer.length > 0) {
+            addNormalButton(normalPageContainer[0]);
+            var subContent = document.querySelector('.content').querySelector('div');
+            continuousObserver.observe(subContent, config);
+            me.disconnect();
+        }
+    });
+    
     var observer = new MutationObserver(function (mutations, me) {
         var content = document.querySelector('.content');
         if (content) {
@@ -51,27 +68,47 @@ var urlType = document.URL.split('/')[4];
     observer.observe(target, config);
 })();
 
-function addButton(a) {
-    var b = document.createElement('div');
-    b.className = 'col-12 col-md-6 col-lg-12';
-    a.appendChild(b);
-    var c = document.createElement('div');
-    c.className = 'p-3 pl-md-1 pl-lg-3 card-block';
-    c.id = 'checkPrice';
-    b.appendChild(c);
-    var d = document.createElement('button');
-    d.className = 'mt-3 cart-btn btn btn-primary btn-lg btn-block';
-    d.id = 'checkPriceButton';
-    d.onclick = checkPrice;
-    e = document.createElement('b');
-    e.id = 'checkPriceButtonText';
-    e.appendChild(document.createTextNode('Check Price'));
-    d.appendChild(e);
-    c.appendChild(d);
-    f = document.createElement('div');
-    f.id = 'checkPriceList';
-    f.className = 'mt-3';
-    c.appendChild(f);
+function addNormalButton(container) {
+    var outsideContainer = document.createElement('div');
+    outsideContainer.className = 'col-12 col-md-6 col-lg-12';
+    container.appendChild(outsideContainer);
+    var priceContainer = document.createElement('div');
+    priceContainer.className = 'p-3 pl-md-1 pl-lg-3 card-block';
+    priceContainer.id = 'checkPrice';
+    outsideContainer.appendChild(priceContainer);
+    var button = document.createElement('button');
+    button.className = 'mt-3 cart-btn btn btn-primary btn-lg btn-block';
+    button.id = 'checkPriceButton';
+    button.onclick = checkPrice;
+    var buttonText = document.createElement('b');
+    buttonText.id = 'checkPriceButtonText';
+    buttonText.appendChild(document.createTextNode('Check Price'));
+    button.appendChild(buttonText);
+    priceContainer.appendChild(button);
+    var checkPriceList = document.createElement('div');
+    checkPriceList.id = 'checkPriceList';
+    checkPriceList.className = 'mt-3';
+    priceContainer.appendChild(checkPriceList);
+}
+
+function addStarDealButton(container) {
+    var priceContainer = document.createElement('div');
+    priceContainer.id = 'checkPrice';
+    container.appendChild(priceContainer);
+    var button = document.createElement('button');
+    button.className = 'cart-btn btn btn-primary btn-lg';
+    button.id = 'checkPriceButton';
+    button.onclick = checkPrice;
+    button.style = 'height: 48px; margin-left: 10px;';
+    var buttonText = document.createElement('b');
+    buttonText.id = 'checkPriceButtonText';
+    buttonText.appendChild(document.createTextNode('Check Price'));
+    button.appendChild(buttonText);
+    priceContainer.appendChild(button);
+    var checkPriceList = document.createElement('div');
+    checkPriceList.id = 'checkPriceList';
+    var title = document.getElementsByClassName('purchase-info-title')[0];
+    title.appendChild(checkPriceList);
 }
 
 function checkPrice() {
@@ -90,7 +127,7 @@ function checkPrice() {
                     for (var j = 0; j < regionList.length; j++) {
                         var priceItem = {
                             'price': bundleData.bundles[i].price[regionList[j]] / 100,
-                            'CNY': 0
+                            'targetCur': 0
                         };
                         bundlePrice[regionList[j]] = priceItem;
                     }
@@ -99,19 +136,20 @@ function checkPrice() {
                 fetchCurrency(buttonText);
             }
         });
-    } else if (urlType == 'game') {
+    } else if (urlType == 'game' || urlType == 'dlc') {
         var gameName = document.URL.split('/')[5];
         GM_xmlhttpRequest({
             method: "GET",
             url: "https://api.fanatical.com/api/products/" + gameName,
             onload: function (response) {
                 var gameData = JSON.parse(response.responseText);
+                var discount = 1 - gameData.current_discount.percent;
                 priceList.length = 0;
                 gamePrice = {};
                 for (var i = 0; i < regionList.length; i++) {
                     var priceItem = {
-                        'price': gameData.price[regionList[i]] / 100,
-                        'CNY': 0
+                        'price': gameData.price[regionList[i]] * discount / 100,
+                        'targetCur': 0
                     };
                     gamePrice[regionList[i]] = priceItem;
                 }
@@ -126,7 +164,7 @@ function fetchCurrency(buttonText) {
     for (var k = 0; k < regionList.length; k++) {
         GM_xmlhttpRequest({
             method: "GET",
-            url: "https://finance.google.cn/finance/converter?a=1&from=" + regionList[k] + "&to=CNY",
+            url: "https://finance.google.cn/finance/converter?a=1&from=" + regionList[k] + "&to=" + targetCur,
             context: {
                 'region': regionList[k],
             },
@@ -138,7 +176,7 @@ function fetchCurrency(buttonText) {
                     buttonText.innerText = 'Finished!';
                     if (urlType == 'bundle') {
                         displayBundleResult();
-                    } else if (urlType == 'game') {
+                    } else if (urlType == 'game' || urlType == 'dlc') {
                         displayGameResult();
                     }
                 }
@@ -158,9 +196,9 @@ function displayBundleResult() {
         var cheapRegion = 'USD';
         for (var j = 0; j < regionList.length; j++) {
             var priceItem = priceList[i][regionList[j]];
-            priceItem.CNY = (curList[regionList[j]] * priceItem.price);
-            if (priceItem.CNY < cheapPrice) {
-                cheapPrice = priceItem.CNY;
+            priceItem.targetCur = (curList[regionList[j]] * priceItem.price);
+            if (priceItem.targetCur < cheapPrice) {
+                cheapPrice = priceItem.targetCur;
                 cheapRegion = regionList[j];
             }
         }
@@ -173,6 +211,7 @@ function displayBundleResult() {
     }
     for (var k = 0; k < priceList.length; k++) {
         var container = document.createElement('div');
+        container.className = 'mt-3';
         c.appendChild(container);
         var titleText = document.createElement('b');
         titleText.appendChild(document.createTextNode('Tier ' + (k + 1).toString()));
@@ -187,7 +226,7 @@ function displayBundleResult() {
                 priceText = document.createElement('span');
             }
             var priceValue = priceList[k][regionList[l]];
-            priceText.appendChild(document.createTextNode(priceValue.price.toFixed(2) + ' ' + regionList[l] + ' = ' + priceValue.CNY.toFixed(2) + ' CNY'));
+            priceText.appendChild(document.createTextNode(priceValue.price.toFixed(2) + ' ' + regionList[l] + ' = ' + priceValue.targetCur.toFixed(2) + ' ' + targetCur));
             container.appendChild(priceText);
             container.appendChild(document.createElement('br'));
         }
@@ -199,9 +238,9 @@ function displayGameResult() {
     var cheapRegion = 'USD';
     for (var j = 0; j < regionList.length; j++) {
         var priceItem = priceList[0][regionList[j]];
-        priceItem.CNY = (curList[regionList[j]] * priceItem.price);
-        if (priceItem.CNY < cheapPrice) {
-            cheapPrice = priceItem.CNY;
+        priceItem.targetCur = (curList[regionList[j]] * priceItem.price);
+        if (priceItem.targetCur < cheapPrice) {
+            cheapPrice = priceItem.targetCur;
             cheapRegion = regionList[j];
         }
     }
@@ -211,6 +250,7 @@ function displayGameResult() {
         c.removeChild(c.firstChild);
     }
     var container = document.createElement('div');
+    container.className = 'mt-3';
     c.appendChild(container);
     for (var l = 0; l < regionList.length; l++) {
         var priceText;
@@ -220,7 +260,7 @@ function displayGameResult() {
             priceText = document.createElement('span');
         }
         var priceValue = priceList[0][regionList[l]];
-        priceText.appendChild(document.createTextNode(priceValue.price.toFixed(2) + ' ' + regionList[l] + ' = ' + priceValue.CNY.toFixed(2) + ' CNY'));
+        priceText.appendChild(document.createTextNode(priceValue.price.toFixed(2) + ' ' + regionList[l] + ' = ' + priceValue.targetCur.toFixed(2) + ' ' + targetCur));
         container.appendChild(priceText);
         container.appendChild(document.createElement('br'));
     }
